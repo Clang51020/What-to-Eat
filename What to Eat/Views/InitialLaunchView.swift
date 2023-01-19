da806c09e1bb6f42946a0e01a6efde98de4b08a9
Chris Lang <chrislang51020@gmail.com>
2023-01-19 17:18:56 -0600

    -Build out variable holders for different stages of the onboarding screens. -Build out AppStorage variable to capture onboarding steps.

--- What to Eat/Views/InitialLaunchView.swift
+++ What to Eat/Views/InitialLaunchView.swift
@@ -18,12 +18,13 @@ struct InitialLaunchView: View {
         
         switch onboardingStep {
         case 0: WelcomeScreen
-        case 1: SplashScreen
-        case 2: SplashScreen
-        case 3: SplashScreen
-        case 4: SplashScreen
-        case 5: SplashScreen
-        default: SplashScreen
+        case 1: SearchOverview
+        case 2: LocationRequest
+        case 3: AddingPlaces
+        case 4: RouletteResults
+        case 5: UserInfoAccountCreation
+        case 6: SplashScreen // replace this with the TabView once that is created.
+        default: WelcomeScreen
         }
     }
 }
@@ -43,6 +44,41 @@ extension InitialLaunchView {
         VStack {
             Image(systemName: "house")
             Text("Welcome Screen")
+        }
+    }
+    
+    var SearchOverview: some View {
+        VStack {
+            Image(systemName: "house")
+            Text("Search Overview")
+        }
+    }
+    
+    var LocationRequest: some View {
+        VStack {
+            Image(systemName: "house")
+            Text("Location Request View")
+        }
+    }
+    
+    var AddingPlaces: some View {
+        VStack {
+            Image(systemName: "house")
+            Text("Adding Places to Lists")
+        }
+    }
+    
+    var RouletteResults: some View {
+        VStack {
+            Image(systemName: "house")
+            Text("Results Request Overview")
+        }
+    }
+    
+    var UserInfoAccountCreation: some View {
+        VStack {
+            Image(systemName: "house")
+            Text("User Information Account Creating Screens")
         }
     }
 }
